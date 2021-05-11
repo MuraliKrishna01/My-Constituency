dec093559adc9e104c7ba460fc0c80c3f655ee1a
Murali Krishna <67138183+MuraliKrishna01@users.noreply.github.com>
2021-05-11 18:44:03 +0530

    Web Development copy 1

---
+++ Adminhome.php
@@ -0,0 +1,133 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link href="assets/css/Profilestyle.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="assets/css/Profilestyle.css">
+       
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+
+    <title>Admin Home</title>
+</head>
+<body>
+<header>
+  
+  <div class="header_bar">
+  <div class="logo-left" id="logo">
+    <a href="#">
+                   <img src="assets/images/MCY title.png" height="160" width="240" margin-left="10px">
+                    
+                </a>
+    </div>
+  
+  </header>
+    <br><br><br><br><br><br><br>
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-wrap -m-4">
+            <div class="p-4 md:w-1/3">
+              <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://images.newindianexpress.com/uploads/user/imagelibrary/2020/8/17/w900X450/roads.jpg" alt="blog">
+                <div class="p-6">
+                  <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Roads</h1>
+                  <p class="leading-relaxed mb-3">Condition of Hyderabad Roads after the heavy rains last week .<br>Location : Begumpet x roads </p>
+                  <div class="flex items-center flex-wrap ">
+                    <a href="https://www.newindianexpress.com/cities/hyderabad/2020/aug/19/watch-out-hyderabad-roads-riddled-with-3000-potholes--2185232.html" class="text-blue-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                      <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M5 12h14"></path>
+                        <path d="M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                    <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                      <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                        <circle cx="12" cy="12" r="3"></circle>
+                      </svg>14
+                    </span>
+                    <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                      <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                      </svg>2
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="p-4 md:w-1/3">
+              <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="http://mief.in/wp-content/uploads/2013/11/govt_school_20111114-300x199.jpg" alt="blog">
+                <div class="p-6">
+                  <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Infastructure</h1>
+                  <p class="leading-relaxed mb-3"> Due to the neglegence of concerned officials the Infastructure of governtment school is in Pity State . <br>Location : Manikonda </p>
+                  <div class="flex items-center flex-wrap">
+                    <a href="https://www.careerindia.com/news/2013/06/28/majority-government-schools-india-lack-infrastructure-005573.html" class="text-blue-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                      <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M5 12h14"></path>
+                        <path d="M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                    <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                      <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                        <circle cx="12" cy="12" r="3"></circle>
+                      </svg>9
+                    </span>
+                    <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                      <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                      </svg>1
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="p-4 md:w-1/3">
+              <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://telanganatoday.com/wp-content/uploads/2017/06/Manjira1.jpg" alt="blog">
+                <div class="p-6">
+                  <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Water</h1>
+                  <p class="leading-relaxed mb-3"> Massive leakage at Manjira water pipe-line .<br> Location: Rajapet colony </p>
+                  <div class="flex items-center flex-wrap ">
+                    <a class="text-blue-500 inline-flex items-center md:mb-2 lg:mb-0"  href="https://telanganatoday.com/massive-leakage-at-manjira-water-pipe-line">Learn More
+                      <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M5 12h14"></path>
+                        <path d="M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                    <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                      <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                        <circle cx="12" cy="12" r="3"></circle>
+                      </svg>7
+                    </span>
+                    <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                      <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                      </svg>3
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+     <div class="container" >
+         <a href="excelsheet.php" >
+            <button class="btn1" >SEE THE COMPLAINTS </button>
+        </a>
+     </div>
+</body>
+</html>
+
---
+++ excelsheet.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link href="Profilestyle.css" rel="stylesheet" type="text/css">
+    <link href=assets/css/home.css rel="stylesheet" type="text/css">
+
+    <title>Excel Sheet </title>
+    <style>
+        .excelsheet{
+          
+          margin-left: auto;
+          margin-right: auto;
+        }
+        /*body{
+          background-image: url(mywebsite/LOGO.jpeg);
+          background-size: cover;
+        }*/
+    </style>
+</head>
+<body>
+<header>
+   
+<div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a href="#">
+                   <img src="assets/images/MCY title.png" height="160" width="240">
+                    
+                </a>
+                <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+                    <a href="#" class="heading 1">Home</a>
+                    <a href="#" class="heading 2">About </a>
+
+                    <a href="#" class="heading 3">Contract Us</a>
+                </nav>
+                <button href=" Adminhome.php " class="inline-flex items-center bg-gray-100 border-2 py-1 px-3 focus:outline-none hover:bg-blue-400 rounded text-base mt-4 md:mt-0">Go Back 
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+            </div>
+  
+    </header> <br><br><br><br><br>
+       <div class="exxcell">
+      <iframe class="excelsheet"
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSSJI-cJuQkCv3bLDOl1kYJrABk65EckqqQiJFQYhwgsOew918BmQXoMcO-TlphfTdbKWaKQo1mJweZ/pubhtml?widget=true&amp;headers=false"  width="1000" height="600" > 
+      </iframe>
+       </div>
+</body>
+</html>
